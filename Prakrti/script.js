35e6ba4ab56d1ea61aa44974494d083d79e44b42
FAIZAL7629 <99400945+FAIZAL7629@users.noreply.github.com>
2022-02-11 17:09:33 +0530

    delete files

--- Prakrti/script.js
+++
@@ -1,8 +0,0 @@
-function muFunction() {
-    var x = document.getElementById("mylink");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else{
-        x.style.display = "block";
-    }
-}
